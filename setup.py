9dfcff8b1223040998f616ebd6c022c8609172aa
Francis Hanna <91334875+frankhanna94@users.noreply.github.com>
2025-12-09 14:13:08 -0500

    Fix package_dir definition in setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 setup(
     name="lca-prommis",
     version="0.1.0",
+    package_dir={"": "src"},    
     packages=find_packages(where="src"),
-    package_dir={"": "src"},
     install_requires=[
         "fedelemflowlist @ git+https://github.com/FLCAC-Admin/fedelemflowlist",
         "netlolca @ git+https://github.com/NETL-RIC/netlolca",
