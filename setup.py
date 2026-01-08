30a16acaaf90f06c8dacfc7736a0177d7cfec160
Francis Hanna <91334875+frankhanna94@users.noreply.github.com>
2026-01-08 14:34:06 -0500

    Fix package_dir in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name="lca-prommis",
     version="0.1.0",
-    package_dir={"src": "src"},    
+    package_dir={"": "src"},    
     packages=["src"] + ["src.create_olca_process"],
     install_requires=[
         "fedelemflowlist @ git+https://github.com/FLCAC-Admin/fedelemflowlist",
